d04a06833ce15cda2812b943c652a75cf527d57a
Bohan <bohan.zp@autonavi.com>
2017-08-18 17:31:17 +0800

    bug fix

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ setup(
     long_description=open('README.md').read(),
     author='ZhangBohan',
     author_email='me@bohanzhang.com',
-    version='0.2.3',
+    version='0.2.4',
     download_url='https://github.com/ZhangBohan/pylint-string-spaces-checkers.git',
     install_requires=[
         'pylint',
     ],
+    packages=['string_spaces_checkers'],
     classifiers=[
         'Intended Audience :: Developers',
         'Environment :: Console',
